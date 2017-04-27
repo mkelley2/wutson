367f63acb1a67b48330efc9ec78e4831af4debbb
mkelley2 <[m_kelley2@yahoo.com]>
2017-04-27 14:15:54 -0700

    add date string for schedule display

--- sites/all/modules/testform/js/testform.js
+++ sites/all/modules/testform/js/testform.js
@@ -6,13 +6,7 @@
     return dat;
   }
   var dat = new Date();
-  var dayminus3;
-  var dayminus2;
-  var dayminus1;
-  var day;
-  var dayplus1;
-  var dayplus2;
-  var dayplus3;
+  var dayminus3, dayminus2, dayminus1, day, dayplus1, dayplus2, dayplus3;
   var arg = 0;
   
   function dateRange(){
@@ -24,31 +18,39 @@
     dayplus2 = dat.addDays(arg+2).toISOString().split("T")[0];
     dayplus3 = dat.addDays(arg+3).toISOString().split("T")[0];
     
+    localeminus3 = dat.addDays(arg-3).toDateString();
+    localeminus2 = dat.addDays(arg-2).toDateString();
+    localeminus1 = dat.addDays(arg-1).toDateString();
+    locale = dat.addDays(arg).toDateString();
+    localeplus1 = dat.addDays(arg+1).toDateString();
+    localeplus2 = dat.addDays(arg+2).toDateString();
+    localeplus3 = dat.addDays(arg+3).toDateString();
+    
     $(".calendar_holder").empty();
     $(".calendar_holder").append(
-      "<div class='row'>" + dayminus3 + "<ul id='" + dayminus3 + "'></ul></div>" +
-      "<div class='row'>" + dayminus2 + "<ul id='" + dayminus2 + "'></ul></div>" +
-      "<div class='row'>" + dayminus1 + "<ul id='" + dayminus1 + "'></ul></div>" +
-      "<div class='row'>" + day + "<ul id='" + day + "'></ul></div>" +
-      "<div class='row'>" + dayplus1 + "<ul id='" + dayplus1 + "'></ul></div>" +
-      "<div class='row'>" + dayplus2 + "<ul id='" + dayplus2 + "'></ul></div>" +
-      "<div class='row'>" + dayplus3 + "<ul id='" + dayplus3 + "'></ul></div>"
+      "<div class='row'>" + localeminus3 + "<ul id='" + dayminus3 + "'></ul></div>" +
+      "<div class='row'>" + localeminus2 + "<ul id='" + dayminus2 + "'></ul></div>" +
+      "<div class='row'>" + localeminus1 + "<ul id='" + dayminus1 + "'></ul></div>" +
+      "<div class='row'>" + locale + "<ul id='" + day + "'></ul></div>" +
+      "<div class='row'>" + localeplus1 + "<ul id='" + dayplus1 + "'></ul></div>" +
+      "<div class='row'>" + localeplus2 + "<ul id='" + dayplus2 + "'></ul></div>" +
+      "<div class='row'>" + localeplus3 + "<ul id='" + dayplus3 + "'></ul></div>"
     );
     episode_info.forEach(function(elem){
       if(elem['Episode Date'] == day){
-        $("#" + day).append("<li>" + elem['Episode Name'] + "</li>");
+        $("#" + day).append("<li>" + elem['Show Name'] + " - " + elem['Episode Name'] + "</li>");
       }else if(elem['Episode Date'] == dayminus1){
-        $("#" + dayminus1).append("<li>" + elem['Episode Name'] + "</li>");
+        $("#" + dayminus1).append("<li>" + elem['Show Name'] + " - " + elem['Episode Name'] + "</li>");
       }else if(elem['Episode Date'] == dayminus2){
-        $("#" + dayminus2).append("<li>" + elem['Episode Name'] + "</li>");
+        $("#" + dayminus2).append("<li>" + elem['Show Name'] + " - " + elem['Episode Name'] + "</li>");
       }else if(elem['Episode Date'] == dayminus3){
-        $("#" + dayminus3).append("<li>" + elem['Episode Name'] + "</li>");
+        $("#" + dayminus3).append("<li>" + elem['Show Name'] + " - " + elem['Episode Name'] + "</li>");
       }else if(elem['Episode Date'] == dayplus1){
-        $("#" + dayplus1).append("<li>" + elem['Episode Name'] + "</li>");
+        $("#" + dayplus1).append("<li>" + elem['Show Name'] + " - " + elem['Episode Name'] + "</li>");
       }else if(elem['Episode Date'] == dayplus2){
-        $("#" + dayplus2).append("<li>" + elem['Episode Name'] + "</li>");
+        $("#" + dayplus2).append("<li>" + elem['Show Name'] + " - " + elem['Episode Name'] + "</li>");
       }else if(elem['Episode Date'] == dayplus3){
-        $("#" + dayplus3).append("<li>" + elem['Episode Name'] + "</li>");
+        $("#" + dayplus3).append("<li>" + elem['Show Name'] + " - " + elem['Episode Name'] + "</li>");
       }
     });
   }
